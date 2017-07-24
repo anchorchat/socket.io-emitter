bb3a4ef285c438f4ff65b3b8b1305a1e2f2004f6
Lars Tadema <lars.tadema@anchor.chat>
2017-07-24 11:35:07 +0200

    added test for multiple rooms

--- test/index.js
+++ test/index.js
@@ -128,6 +128,49 @@ describe('emitter', function() {
       });
     });
 
+    it('should be able to emit to multiple rooms', function(done){
+      var pub = redis.createClient();
+      var sub = redis.createClient(null, null, {return_buffers: true});
+      srv = http();
+      var sio = io(srv, {adapter: redisAdapter({pubClient: pub, subClient: sub})});
+
+      var secondConnecting = false;
+      srv.listen(function() {
+        sio.on('connection', function(socket) {
+          if (secondConnecting) {
+            socket.join('exclusive room');
+            socket.join('other exclusive room');
+          } else {
+            secondConnecting = true;
+          }
+
+          socket.on('broadcast event', function(payload) {
+            socket.emit('broadcast event', payload);
+          });
+        });
+      });
+
+      var a = client(srv, { forceNew: true });
+      a.on('broadcast event', function(payload) {
+        expect().fail();
+      });
+
+      var b;
+      a.on('connect', function() {
+        b = client(srv, { forceNew: true });
+
+        b.on('broadcast event', function(payload) {
+          expect(payload).to.be('broadcast payload');
+          setTimeout(done, 1000);
+        });
+
+        b.on('connect', function() {
+          var emitter = ioe({ host: 'localhost', port: '6379' });
+          emitter.to(['exclusive room', 'other exclusive room']).broadcast.emit('broadcast event', 'broadcast payload');
+        });
+      });
+    });
+
     it('should be able to emit to a socket by id', function(done){
       var pub = redis.createClient();
       var sub = redis.createClient(null, null, {return_buffers: true});
